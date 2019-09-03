6cd32ec60e58be1fa99a3a97ad0d6a2c578ae6da
Kristan Johns <kristandjohns@gmail.com>
2019-09-03 23:37:09 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ rubies = YAML.load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
   latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
 
 RSpec.configure do |config|
-
+ config.expect_with :rspec do |expectations|
   def __
     # raise "replace underscore with code"
   end
